383a430b8cd9efa23a61de8ac94252ab19265bb2
AmedeoMignano <amedeo56@gmail.com>
2025-11-12 11:47:15 +0100

    Add Main Services

--- BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/SecurityConfig.java
+++ BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5500"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5500","http://localhost:5174" ));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
