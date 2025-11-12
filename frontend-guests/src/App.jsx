83e3ed82d967ad6b300b3149f2a298b4f5b7ae9d
AmedeoMignano <amedeo56@gmail.com>
2025-11-12 12:58:45 +0100

    Add-Guest-home page

--- frontend-guests/src/App.jsx
+++ frontend-guests/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GuestHome from "./components/GuestHome";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GuestHome />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
