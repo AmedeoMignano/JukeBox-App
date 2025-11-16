39bd5f7020ef1bfdc6e85df5b1b6e1488ab44981
AmedeoMignano <amedeo56@gmail.com>
2025-11-16 12:07:39 +0100

    Preparing For Deploying BackEnd

--- BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/WebSocketConfig.java
+++ BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/WebSocketConfig.java
@@ -14,6 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
        registry.addEndpoint("/ws").
                setAllowedOrigins("http://127.0.0.1:5500", "http://localhost:5173",  "http://localhost:5174").
+               setAllowedOriginPatterns("*").
                withSockJS();
     }
 
