67d8f9443db3d5a227a848606f5490c56a4c710b
AmedeoMignano <amedeo56@gmail.com>
2025-11-16 12:27:08 +0100

    adding Url

--- BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/SecurityConfig.java
+++ BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5500","http://localhost:5174" ));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5500","http://localhost:5174", "https://rich-sheelagh-amedeomignano-0e8df352.koyeb.app/"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
