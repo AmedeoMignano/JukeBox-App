f12fb1c6a6ab5b37c6a882f27216b6702439ac80
AmedeoMignano <amedeo56@gmail.com>
2025-11-18 12:50:34 +0100

    Fix BE

--- BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/WebSocketConfig.java
+++ BackEnd/src/main/java/amedeo/mignano/jukebox_app/security/WebSocketConfig.java
@@ -13,8 +13,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
        registry.addEndpoint("/ws").
-               setAllowedOrigins("http://127.0.0.1:5500", "http://localhost:5173",  "http://localhost:5174").
-               setAllowedOriginPatterns("*").
+               setAllowedOrigins(  "http://localhost:5174",
+                       "http://localhost:5173",
+                       "http://127.0.0.1:5500",
+                       "https://rich-sheelagh-amedeomignano-0e8df352.koyeb.app").
                withSockJS();
     }
 
